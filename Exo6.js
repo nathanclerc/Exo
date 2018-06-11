a821192d18621ecf8c1d05f4d5b9d0b613d1d36b
SIMOCCAuch19 <clerc6nathan@hotmail.fr>
2018-06-11 15:12:03 +0200

    Les derniers exo

--- Exo6.js
+++ Exo6.js
@@ -1,6 +1,20 @@
-var nb1 = prompt("Choisi un nombre à virgule");
+/*var nb1 = prompt("Choisi un nombre à virgule");
 var nb2 = prompt("Choisi un deuxième nombre à virgule");
 nb1=parseInt(nb1);
 nb2=parseInt(nb2);
 var result = nb1 % nb2;
 alert(result);
+
+//Exo 7
+function calcul(){
+	var resultat =prompt("Dis ta pointure")*2 +5 *50 -prompt("T'es né quand?")+1766;
+	alert(resultat);
+}
+*/
+
+var age= prompt("Tu a quel âge?");
+if (age >=18) {
+	alert("Vous êtes majeur!");
+}else{
+	alert("Vous êtes mineur!");
+}
