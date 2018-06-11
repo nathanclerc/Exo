1dfc391a6a000861491ed4777ffa218408b027d7
SIMOCCAuch19 <clerc6nathan@hotmail.fr>
2018-06-11 13:57:41 +0200

    Exo 4

--- Exo.js
+++ Exo.js
@@ -11,5 +11,11 @@ alert("Nom: " + nom + "\nPrenom: " + prenom + "\nVille: " + ville);
 //Exo 3
 var prenom2 = prompt("Quel est ton prénom?");
 alert("Bonjour, " + prenom2);
-*/
+
 //Exo 4
+var nom1= prompt("Quel est ton nom?");
+var prenom1 = prompt("Quel est ton prenom?");
+var ville1 = prompt("Où habite-tu?");
+alert("Bonjour, "+ prenom1 + " " + nom1 + " tu habite à " + ville1);
+*/
+//Exo 5
