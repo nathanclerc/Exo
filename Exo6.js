650e6a9eb218390127b68680626d74ae6b7321e7
SIMOCCAuch19 <clerc6nathan@hotmail.fr>
2018-06-12 11:23:30 +0200

    Exo tableau

--- Exo6.js
+++ Exo6.js
@@ -1,4 +1,5 @@
-/*var nb1 = prompt("Choisi un nombre à virgule");
+/*Exo 6
+var nb1 = prompt("Choisi un nombre à virgule");
 var nb2 = prompt("Choisi un deuxième nombre à virgule");
 nb1=parseInt(nb1);
 nb2=parseInt(nb2);
--- JS-exercice-master/app.js
+++ JS-exercice-master/app.js
@@ -1,4 +1,25 @@
 var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
-var month = ["janvier","fevrier","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","dececmbre"];
-
-alert(month[2]);
+//var month = ["janvier","fevrier","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","dececmbre"];
+//Exo2
+//alert(month[2]);
+//Exo 3
+//alert(month[4]);
+//Exo 4
+//alert(month[10]);
+//Exo 5
+//month.splice(7, 1, "Août");
+//alert(month[7]);
+//Exo 6
+//document.write(month);
+//Exo 8
+//array.splice(2, 0, "Courgette");
+//alert(array[2]);
+//Exo 9
+//array.splice(4, 0, "Citron");
+//alert(array);
+//Exo 10
+//array.splice(1,1);
+//alert(array);
+//Exo 11
+array.splice(2, 0, "Poire");
+alert(array);
