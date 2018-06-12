aae568c1d27ead4156a6ef0b8c98e1251ebc4aeb
SIMOCCAuch19 <clerc6nathan@hotmail.fr>
2018-06-12 16:26:00 +0200

    exo loop fini

---
+++ JS-exercice-master/loop/apploop.js
@@ -0,0 +1,53 @@
+//Exo 1
+/*
+var maVar=0;
+for (var maVar = 0; maVar <= 10; maVar ++) {
+	document.write(maVar);
+	}
+//Exo 2
+var nb1=0;
+var nb2=100;
+for (var nb1; nb1 <= 20; nb1++) {
+	var res = nb1 * nb2;
+	document.write(res + "||");
+}
+//Exo 3
+var nb1 = 100;
+
+var nb2 = 15;
+
+    for (nb1=100; nb1 >=10 ; nb1--) { 
+
+        var res = nb1 * nb2;
+
+        document.write(res + "||");
+
+    }
+
+//Exo 4
+var nb1 = 1;
+while(nb1<10){
+	document.write(nb1 + "||");
+	nb1=nb1+ nb1/2;
+}
+
+//Exo 5
+var nb1=1;
+for (var nb1= 1; nb1< 15; nb1++) {
+	document.write("On y arrive presque\n");
+}
+//Exo 6
+var nb1=20;
+for (var nb1= 20; nb1>0; nb1--) {
+	document.write("C'est presque bon\n");
+}
+//Exo 7
+var nb1=1;
+for (var nb1= 1; nb1<100; nb1+=15) {
+	document.write("On tient le bon bout\n");
+}*/
+//Exo 8
+var nb1=0;
+for (var nb1= 0; nb1<200; nb1+=12) {
+	document.write("Enfin!!!!\n");
+}
