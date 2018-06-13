a7b71f3ddac01bfce46d78836e3d0d1201a22d2b
SIMOCCAuch19 <clerc6nathan@hotmail.fr>
2018-06-13 11:53:12 +0200

    exo1 fini

---
+++ js-exercises-base2-master/exercice 1/app.js
@@ -0,0 +1,9 @@
+function onHover()
+{
+   document.getElementById('image1').setAttribute('src', 'images/image1_2.jpg');
+}
+
+function offHover()
+{
+    document.getElementById('image1').setAttribute('src', 'images/image1.jpg');
+}
